937dc6a0bd3af68ddbc131bdd4adda6e4817e93f
Nick Saindon <ntimeproductions@gmail.com>
2025-01-12 01:56:39 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import "@/styles/globals.css";
+import { useEffect } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/globals.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />;
 }
